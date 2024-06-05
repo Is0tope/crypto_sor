e1bc5c85a160135889a250c21c7c20a056f8a245
Kris Machowski <kris.machowski@gmail.com>
2024-06-05 23:36:29 +0800

    Set explicit address

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,36 +3,36 @@ import routes from './routes'
 import { SOR } from './app'
 import cors from 'fastify-cors'
 import prettifier from '@mgcrea/pino-pretty-compact'
-import fastifyRequestLogger from '@mgcrea/fastify-request-logger';
+import fastifyRequestLogger from '@mgcrea/fastify-request-logger'
 
 const PORT = process.env.PORT || 8080
 const VACUUM_INTERVAL = 10_000
 const RECONNECT_INTERVAL = 30 * 60_000
 
 const server = fastify({
-    disableRequestLogging: true,
-    logger: {
-        prettyPrint: true, 
-        prettifier 
-    }
+  disableRequestLogging: true,
+  logger: {
+    prettyPrint: true,
+    prettifier,
+  },
 })
 
 server.register(cors)
-server.register(fastifyRequestLogger);
+server.register(fastifyRequestLogger)
 
 routes.forEach((r: any) => server.route(r))
 
-server.listen(PORT, (err, address) => {
-    if (err) {
-      console.error(err)
-      process.exit(1)
-    }
+server.listen(PORT, '0.0.0.0', (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
 })
 
 setInterval(() => {
-    SOR.vacuum()
+  SOR.vacuum()
 }, VACUUM_INTERVAL)
 
 setInterval(() => {
-    SOR.reconnect()
+  SOR.reconnect()
 }, RECONNECT_INTERVAL)
